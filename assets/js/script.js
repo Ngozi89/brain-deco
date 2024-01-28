270d84cd442313b85b5c38974d2bcccd5c2c4acd
Ngozi Omenka <ngozi.madu@live.se>
2024-01-28 23:26:32 +0000

    Activated start game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("Connected")
+// Add all required elements
+let start_game = document.querySelector(".start_game button");
+let game_info_list = document.querySelector(".game_info_list");
+let exit_btn = game_info_list.querySelector(".buttons .exit");
+let continue_btn = game_info_list.querySelector(".buttons .continue");
+
+start_game.onclick = ()=>{
+    game_info_list.classList.add("activeInfo");
+}
