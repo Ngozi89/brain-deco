3749e8e0f3cc9894169e25570e408438b57a395c
Ngozi Omenka <ngozi.madu@live.se>
2024-01-28 23:48:29 +0000

    Activated exit and contiunebuttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,18 @@ let start_game = document.querySelector(".start_game button");
 let game_info_list = document.querySelector(".game_info_list");
 let exit_btn = game_info_list.querySelector(".buttons .exit");
 let continue_btn = game_info_list.querySelector(".buttons .continue");
+let quiz_box = game_info_list.querySelector("quiz_box");
 
+// Activate start game button
 start_game.onclick = ()=>{
     game_info_list.classList.add("activeInfo");
+};
+
+exit_btn.onclick = ()=>{
+    game_info_list.classList.remove("activeInfo");
+}
+
+continue_btn.onclick = () => {
+    game_info_list.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
 }
