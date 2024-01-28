24310ef87c086588334f455af8e53e26e9d201e2
Ngozi Omenka <ngozi.madu@live.se>
2024-01-28 04:36:49 +0000

    Added quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,123 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What country has the highest life expectancy?",
+        answer: "Hong Kong",
+        options: [
+            "United State",
+            "Italy",
+            "Malaysia",
+            "Hong Kong"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which langauge has the more native speakers?",
+        answer: "Spanish",
+        options: [
+            "English",
+            "Spanish",
+            "French",
+            "Chinese"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What disease commonly spread on pirate ships?",
+        answer: "Scurvy",
+        options: [
+            "Maleria",
+            "Corona-virus",
+            "Scurvy",
+            "Flu"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What year was the United Nations established?",
+        answer: "1945",
+        options: [
+            "1960",
+            "1975",
+            "1945",
+            "1990"
+        ]
+    },
+    {
+        numb: 4,
+        question: "How many minutes are in a full week?",
+        answer: "10,080",
+        options: [
+            "10,100",
+            "10,799",
+            "10,107",
+            "10080"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Aureolin is a shade of what color?",
+        answer: "Yellow",
+        options: [
+            "Red",
+            "White",
+            "Yellow",
+            "Green"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which planet has the most moons?",
+        answer: "Saturn",
+        options: [
+            "Mercury",
+            "Saturn",
+            "Earth",
+            "Mars"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What country has won the most World Cups?",
+        answer: "Saturn",
+        options: [
+            "Brazil",
+            "Germany",
+            "United States",
+            "Spain"
+        ]
+    },
+    {
+        numb: 8,
+        question: "In what country was Elon Musk born?",
+        answer: "South Africa",
+        options: [
+            "Austria",
+            "South Africa",
+            "United States",
+            "United Kingdom"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Where is the strongest human muscle located?",
+        answer: "Jaw",
+        options: [
+            "Hand",
+            "Jaw",
+            "Leg",
+            "Tongue"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many hearts does an octopus have?",
+        answer: "Jaw",
+        options: [
+            "4",
+            "2",
+            "1",
+            "3"
+        ]
+    },
+]
