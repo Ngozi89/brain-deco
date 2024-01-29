59d17b5f5b03662d27d7cf9cf4b2d92b5a7fa405
Ngozi Omenka <ngozi.madu@live.se>
2024-01-29 06:36:14 +0000

    Added quiz result section

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,30 @@ exit_btn.onclick = ()=>{
 continue_btn.onclick = ()=>{
     game_info_list.classList.remove("activeInfo");
     quiz_box.classList.add("activeQuiz");
-    showQuestions();
+    showQuestions(0);
 }
 
 let que_count = 0;
 
+let next_btn = document.querySelector(".next_btn")
+
+next_btn.onclick = ()=>{
+    if (que_count < questions.length - 1){
+        que_count++;
+        showQuestions(que_count);
+    }else{
+        console.log("You have completed the questions")
+    }
+}
+
 function showQuestions(index){
-    let que_text = document.querySelector(".que_test")
-    let que_tag = "<h3>"+ questions[3].question +"</h3>";
+    let que_text = document.querySelector(".que_test");
+    let option_list = document.querySelector(".option_list");
+    let que_tag = '<span>'+ questions[index].question +'</span>';
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
     que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag
 }
