17c796369222a956d7677d31fb9e868c2ed1639f
Ngozi Omenka <ngozi.madu@live.se>
2024-01-29 01:24:41 +0000

    Added quiz qustion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let start_game = document.querySelector(".start_game button");
 let game_info_list = document.querySelector(".game_info_list");
 let exit_btn = game_info_list.querySelector(".buttons .exit");
 let continue_btn = game_info_list.querySelector(".buttons .continue");
-let quiz_box = game_info_list.querySelector("quiz_box");
+let quiz_box = document.querySelector(".quiz_box");
 
 // Activate start game button
 start_game.onclick = ()=>{
@@ -14,7 +14,16 @@ exit_btn.onclick = ()=>{
     game_info_list.classList.remove("activeInfo");
 }
 
-continue_btn.onclick = () => {
+continue_btn.onclick = ()=>{
     game_info_list.classList.remove("activeInfo");
     quiz_box.classList.add("activeQuiz");
+    showQuestions();
+}
+
+let que_count = 0;
+
+function showQuestions(index){
+    let que_text = document.querySelector(".que_test")
+    let que_tag = "<h3>"+ questions[3].question +"</h3>";
+    que_text.innerHTML = que_tag;
 }
